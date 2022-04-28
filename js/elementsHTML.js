a433993f40c6001e268fdbca913c41400c854136
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-04-28 01:20:54 -0300

    Iniciando javascript, criando arquivo que contém elementos HTML que serão manipulados pelo javascript

---
+++ js/elementsHTML.js
@@ -0,0 +1,25 @@
+const minutesDisplay = document.querySelector("#minutes");
+const secondsDisplay = document.querySelector("#seconds");
+const playButton = document.querySelector("#playButton");
+const pauseButton = document.querySelector("#pauseButton");
+const stopButton = document.querySelector("#stopButton");
+const addButton = document.querySelector("#addButton");
+const subtractButton = document.querySelector("#subtractButton");
+const forestCard = document.querySelector("#forestCard");
+const rainCard = document.querySelector("#rainCard");
+const coffeShopCard = document.querySelector("#coffeShopCard");
+const firePlaceCard = document.querySelector("#firePlaceCard");
+
+export {
+  minutesDisplay,
+  secondsDisplay,
+  playButton,
+  pauseButton,
+  stopButton,
+  addButton,
+  subtractButton,
+  forestCard,
+  rainCard,
+  coffeShopCard,
+  firePlaceCard,
+};
---
+++ js/index.js
@@ -0,0 +1,13 @@
+import {
+  minutesDisplay,
+  secondsDisplay,
+  playButton,
+  pauseButton,
+  stopButton,
+  addButton,
+  subtractButton,
+  forestCard,
+  rainCard,
+  coffeShopCard,
+  firePlaceCard,
+} from "./elementsHTML.js";
