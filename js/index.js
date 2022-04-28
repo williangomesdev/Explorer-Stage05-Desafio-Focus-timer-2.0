6e4d486a4ade41e10b2b4e58b46bfbfcd4905850
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-04-28 18:28:47 -0300

    refatorando funções

--- js/index.js
+++ js/index.js
@@ -12,27 +12,77 @@ import {
   firePlaceCard,
 } from "./htmlElements.js";
 
+//count é usado no contador de minutos
 let count = 0;
+
+//Reiniciar os botões de modo padrão aós finalizar o cronometro
+function resetControls() {
+  playButton.classList.remove("hide");
+  pauseButton.classList.add("hide");
+  addButton.removeAttribute("disabled");
+  subtractButton.removeAttribute("disabled");
+}
+
+//atualização dos minutos no minutos no display
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0");
+  secondsDisplay.textContent = String(seconds).padStart(2, "0");
+}
+
+//diminui o seconds a cada 1 segundo, função recursiva
+//Acada segundo em 0 minuto diminui em 1
+function countDownSeconds() {
+  setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent);
+    let minutes = Number(minutesDisplay.textContent);
+
+    /*  secondsDisplay.textContent = "00"; */
+    updateTimerDisplay(minutes, 0);
+
+    if (minutes <= 0) {
+      resetControls();
+      return;
+    }
+
+    if (seconds <= 0) {
+      seconds = 2;
+      --minutes;
+
+      /* minutesDisplay.textContent = String(minutes - 1).padStart(2, "0"); */
+      /*  updateTimerDisplay(String(minutes - 1), seconds); */
+    }
+    /* secondsDisplay.textContent = String(seconds - 1).padStart(2, "0"); */
+    updateTimerDisplay(minutes, String(seconds - 1));
+    countDownSeconds();
+  }, 1000);
+}
 
 //Evento botão play/pause
 playButton.addEventListener("click", function () {
   playButton.classList.add("hide");
   pauseButton.classList.remove("hide");
+  addButton.setAttribute("disabled", "disabled");
+  subtractButton.setAttribute("disabled", "disabled");
+
+  countDownSeconds();
 });
 
 pauseButton.addEventListener("click", function () {
-  pauseButton.classList.add("hide");
   playButton.classList.remove("hide");
+  pauseButton.classList.add("hide");
+  addButton.removeAttribute("disabled");
+  subtractButton.removeAttribute("disabled");
 });
 
 stopButton.addEventListener("click", function () {
-  pauseButton.classList.add("hide");
-  playButton.classList.remove("hide");
+  resetControls();
 });
 
 addButton.addEventListener("click", function () {
   if (count <= 90) {
     count = count + 5;
+  } else {
+    addButton.disable = true;
   }
 
   minutesDisplay.textContent = String(count).padStart(2, "0");
