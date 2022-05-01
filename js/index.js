787bbd1d25a3fc801a6c20a7f1bc0e5d282a75e3
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-05-01 16:53:40 -0300

    Desestruturando elementos HTML

--- js/index.js
+++ js/index.js
@@ -1,25 +1,28 @@
+import { elements } from "./htmlElements.js";
 import Controls from "./controls.js";
 import Timer from "./timer.js";
 import SoundCards from "./soundCards.js";
 
-const minutesDisplay = document.querySelector("#minutes");
-const secondsDisplay = document.querySelector("#seconds");
-const playButton = document.querySelector("#playButton");
-const pauseButton = document.querySelector("#pauseButton");
-const stopButton = document.querySelector("#stopButton");
-const addButton = document.querySelector("#addButton");
-const subtractButton = document.querySelector("#subtractButton");
-const forestCard = document.querySelector("#forestCard");
-const forestSound = document.querySelector("#forestSound");
-const rainCard = document.querySelector("#rainCard");
-const rainSound = document.querySelector("#rainSound");
-const coffeShopCard = document.querySelector("#coffeShopCard");
-const coffeShopSound = document.querySelector("#coffeShopSound");
-const firePlaceCard = document.querySelector("#firePlaceCard");
-const firePlaceSound = document.querySelector("#firePlaceSound");
-const standardMinutes = 0;
-const standardSeconds = 0;
-let count = 0;
+const {
+  minutesDisplay,
+  secondsDisplay,
+  playButton,
+  pauseButton,
+  stopButton,
+  addButton,
+  subtractButton,
+  forestCard,
+  forestSound,
+  rainCard,
+  rainSound,
+  coffeShopCard,
+  coffeShopSound,
+  firePlaceCard,
+  firePlaceSound,
+  standardMinutes,
+  standardSeconds,
+  count,
+} = elements;
 
 const controls = Controls({
   playButton,
